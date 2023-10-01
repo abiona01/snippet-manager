00f85753e0877790cb7a3be86f75fa42981e0ef1
abiona01 <quadratwemimo@gmail.com>
2023-10-01 15:10:45 +0100

    backend deployment setup

--- server-end/server.js
+++ server-end/server.js
@@ -1,33 +1,28 @@
-require("dotenv").config();
-const express = require("express");
-const mongoose = require("mongoose");
-const colors = require("colors");
-const cors = require("cors");
-const cookieParser = require("cookie-parser");
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const colors = require('colors');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const app = express();
 app.use(express.json());
-app.use(
-	cors({
-		origin: "http://localhost:3000",
-		credentials: true,
-	})
-);
+app.use(cors());
 app.use(cookieParser());
 
 //set up routers
-app.use("/snippet", require("./routers/snippetRouter"));
-app.use("/auth", require("./routers/userRouter"));
+app.use('/snippet', require('./routers/snippetRouter'));
+app.use('/auth', require('./routers/userRouter'));
 
 //connect to DB
 mongoose.connect(
-	process.env.MONGO_URI,
-	{
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	},
-	console.log("MongoDB connection SUCCESS".cyan.underline.bold),
-	(err) => {
-		if (err) return console.log(err);
-	}
+  process.env.MONGO_URL,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  console.log('MongoDB connection SUCCESS'.cyan.underline.bold),
+  (err) => {
+    if (err) return console.log(err);
+  }
 );
-app.listen(5000, () => console.log("Server stated on port 5000".yellow.bold));
+app.listen(5500, () => console.log('Server stated on port 5000'.yellow.bold));
